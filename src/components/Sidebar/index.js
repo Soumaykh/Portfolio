638dd4793e3594c2163ee4cc8e63a271ab13ca02
Soumay Khandelwal <soumaykhandelwal@gmail.com>
2025-06-27 12:28:05 +0530

    Added responsive mobile ui

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,44 +2,66 @@ import './index.scss'
 import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub2.png'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faEnvelope, faHome,faUser} from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faClose, faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
 
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
 
-const Sidebar = () =>(
-
+  return (
     <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={LogoS} alt="logo"/>
-            <img className="sub-logo" src={LogoSubtitle} alt="soumay"/>
-        </Link>
+      <Link className='logo' to='/'>
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="soumay" />
+      </Link>
 
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
-            </NavLink>
-        </nav>
-        <ul>
-            <li>
-                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/soumay-khandelwal-144737274'>
-                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel='noreferrer' href='https://github.com/Soumaykh'>
-                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-                </a>
-            </li>
-            
-        </ul>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink exact="true" activeclassname="active" to="/" onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about" onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact" onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+
+        <FontAwesomeIcon 
+        onClick={() => setShowNav(false)}
+        icon={faClose}
+        color='#ffd700'
+        size='3x'
+        className='close-icon'
+        />
+      </nav>
+
+      <ul>
+        <li>
+          <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/soumay-khandelwal-144737274'>
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel='noreferrer' href='https://github.com/Soumaykh'>
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+
+      {/* Hamburger icon to show nav */}
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size='3x'
+        className='hamburger-icon'
+      />
+
+    
     </div>
-)
+  )
+}
 
 export default Sidebar
